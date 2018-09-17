75e5d9f46a458de6bbf8a73978738d2c03d4f602
Edson Machado <edson.machado@grupocmc.es>
2018-09-17 13:37:42 +0200

    Adding web3, config and entering the lottery

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import lottery from './lottery';
+import web3 from './web3';
 
 class App extends Component {
+
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: '',
+    message: ''
+  };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...'});
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: 'You have been entered!'});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h2>Lottery contract</h2>
+        <p>
+          This is a contract managed by {this.state.manager}.
+          <br/>
+          There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
+        
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input 
+              type="text"
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })} />
+          </div>
+          <button>Enter</button>
+        </form>
+
+        <hr/>
+
+        <h1>{this.state.message}</h1>
+
       </div>
     );
   }
+
 }
 
 export default App;
